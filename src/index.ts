5166572154b5efd3b634cf5dac176650cfaca0ad
Daniel Amsel <lesmaleinad@hotmail.com>
2021-10-18 19:46:56 -0600

    Add card playing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export enum Suite {
+export enum Suit {
     Hearts = 'H',
     Diamonds = 'D',
     Clubs = 'C',
@@ -23,39 +23,82 @@ export const valueToPoints: { [key in Value]: number } = {
     [Value.Nine]: 0,
 };
 
+export function cardCompare(c1: Card, c2: Card, led?: Suit, trump?: Suit) {
+    if ((c1.suit === trump) !== (c2.suit === trump)) {
+        return c2.suit === trump ? 1 : -1;
+    }
+
+    if ((c1.suit === led) !== (c2.suit === led)) {
+        return c2.suit === led ? 1 : -1;
+    }
+
+    return valueToPoints[c2.value] - valueToPoints[c1.value];
+}
+
+export function equalCards(c1: Card, c2: Card): boolean {
+    return c1.value === c2.value && c1.suit === c2.suit;
+}
+
 export interface Card {
-    suite: Suite;
+    suit: Suit;
     value: Value;
 }
 
 export const allCards: Card[] = (() => {
     const cards: Card[] = [];
-    for (const suite of Object.values(Suite)) {
+    for (const suit of Object.values(Suit)) {
         for (const value of Object.values(Value)) {
-            cards.push({ suite, value });
+            cards.push({ suit, value });
         }
     }
     return cards;
 })();
 
-class Player {
+export interface CardPot {
+    suit: Suit | undefined;
+    cards: Card[];
+}
+
+export class Player {
     private _cards: readonly Card[];
     public get cards(): readonly Card[] {
         return this._cards;
     }
 
+    public score: number = 0;
+
     constructor(initalCards: readonly Card[] = []) {
         this._cards = initalCards;
     }
+
+    public removeCard(card: Card) {
+        this._cards = this.cards.filter((c) => !equalCards(c, card));
+    }
+
+    public toString() {
+        return this.cards.map((c) => c.suit + c.value).join(',');
+    }
 }
 
+export type PlayerIndex = 0 | 1 | 2;
+
 export class Tysiac {
-    public readonly players: readonly [Player, Player, Player];
-    public currentLeader: Player;
+    public players: readonly [Player, Player, Player];
+
+    public readonly cardPot: CardPot = { cards: [], suit: undefined };
+    private _currentTrump?: Suit;
+    public get currentTrump(): Suit | undefined {
+        return this._currentTrump;
+    }
+    private _currentPlayerIndex: PlayerIndex;
+    public get currentPlayer(): Player {
+        return this.players[this._currentPlayerIndex];
+    }
 
     constructor() {
         this.players = this.initPlayers();
-        this.currentLeader = this.players[0];
+        // this.currentLeader = this.players[0];
+        this._currentPlayerIndex = 0;
     }
 
     private initPlayers(): readonly [Player, Player, Player] {
@@ -76,13 +119,114 @@ export class Tysiac {
         return array;
     }
 
+    private getWinningCardIndex(cards: Card[], trump?: Suit): PlayerIndex {
+        if (cards.length !== 3) {
+            console.error(this, cards, trump);
+            throw new Error('Winning card decided from more than 3');
+        }
+
+        let winningIndex: PlayerIndex = 0;
+        const ledSuit = cards[0]!.suit;
+
+        for (const i of [1, 2] as const) {
+            if (
+                cardCompare(cards[winningIndex]!, cards[i]!, ledSuit, trump) > 0
+            ) {
+                winningIndex = i;
+            }
+        }
+
+        return winningIndex;
+    }
+
+    // private getPlayerIndex(player: Player): 0 | 1 | 2 {
+    //     for (const index of [0, 1, 2] as const) {
+    //         if (this.players[index] === player) {
+    //             return index;
+    //         }
+    //     }
+
+    //     console.error(this.players, player);
+    //     throw new Error('Invalid player searched');
+    // }
+
     public getCardOwner(lookupCard: Card): Player | undefined {
         return this.players.find((player) =>
-            player.cards.some(
-                (card) =>
-                    card.value === lookupCard.value &&
-                    card.suite === lookupCard.suite
-            )
+            player.cards.some((card) => equalCards(card, lookupCard))
+        );
+    }
+
+    private checkRoundFinished() {
+        if (this.cardPot.cards.length === 3) {
+            this.finishRound();
+        } else {
+            this._currentPlayerIndex++;
+
+            if (this._currentPlayerIndex > 2) {
+                console.error(this);
+                throw new Error('Too many turns have passed this round');
+            }
+        }
+    }
+
+    private finishRound() {
+        const roundCards = this.cardPot.cards;
+        this.clearPot(this.cardPot);
+
+        const potPoints = roundCards.reduce(
+            (total, c) => total + valueToPoints[c.value],
+            0
+        );
+
+        const winningPlayerIndex = this.getWinningCardIndex(
+            roundCards,
+            this.currentTrump
+        );
+
+        this.players[winningPlayerIndex].score += potPoints;
+        this.setFirstPlayer(winningPlayerIndex);
+    }
+
+    private setFirstPlayer(playerIndex: PlayerIndex) {
+        const [p1, p2, p3] = this.players;
+        switch (playerIndex) {
+            case 2:
+                this.players = [p3, p1, p2];
+                break;
+            case 1:
+                this.players = [p2, p3, p1];
+            case 0:
+                break;
+        }
+        this._currentPlayerIndex = 0;
+    }
+
+    private clearPot(
+        cardPot: CardPot
+    ): asserts cardPot is { cards: []; suit: undefined } {
+        cardPot.cards = [];
+        cardPot.suit = undefined;
+    }
+
+    public sendMove(card: Card) {
+        if (this.isValidMove(card)) {
+            this.cardPot.suit ??= card.suit;
+            this.cardPot.cards.push(card);
+            this.currentPlayer.removeCard(card);
+            this.checkRoundFinished();
+        } else {
+            console.error('Invalid move', this, card);
+        }
+    }
+
+    public isValidMove(card: Card): boolean {
+        const cardSuit = card.suit;
+        const currentSuit = this.cardPot.suit;
+        return (
+            this.currentPlayer.cards.some((c) => equalCards(c, card)) &&
+            (!currentSuit ||
+                cardSuit === currentSuit ||
+                this.currentPlayer.cards.every((c) => c.suit !== currentSuit))
         );
     }
 }
--- src/tysiac.test.ts
+++ src/tysiac.test.ts
@@ -1,4 +1,8 @@
-import { allCards, Tysiac } from './index';
+import { allCards, Card, cardCompare, Suit, Tysiac } from './index';
+
+function assertTruthy<T>(thing: T | undefined | null): asserts thing is T {
+    expect(thing).toBeTruthy();
+}
 
 test('Tysiac should deal every card, 8 to each player', () => {
     const game = new Tysiac();
@@ -14,5 +18,60 @@ test('Tysiac should deal every card, 8 to each player', () => {
 
 test('Tysiac should allow players to play their cards', () => {
     const game = new Tysiac();
-    expect(game.currentLeader).toBe(game.players[0]);
+    const originalPlayers = game.players;
+    const [p1, p2, p3] = originalPlayers;
+
+    function findMostValuableCard(cards: readonly Card[]): Card {
+        const card = cards.slice().sort(cardCompare)[0];
+        assertTruthy(card);
+        return card;
+    }
+
+    function findValidCard(cards: readonly Card[], suit?: Suit): Card {
+        const suitCards = suit ? cards.filter((c) => c.suit === suit) : cards;
+        return findMostValuableCard(suitCards.length === 0 ? cards : suitCards);
+    }
+
+    expect(game.currentPlayer).toBe(p1);
+    expect(game.currentTrump).toBe(undefined);
+
+    const p1HighestValueCard = findMostValuableCard(p1.cards);
+    game.sendMove(p1HighestValueCard);
+    expect(game.cardPot.cards).toContain(p1HighestValueCard);
+    expect(game.cardPot.suit).toBe(p1HighestValueCard.suit);
+    expect(p1.cards).not.toContain(p1HighestValueCard);
+    expect(game.currentPlayer).toBe(p2);
+
+    const nextP2Card = findValidCard(p2.cards, game.cardPot.suit);
+
+    game.sendMove(nextP2Card);
+    expect(game.cardPot.cards).toContain(p1HighestValueCard);
+    expect(game.cardPot.cards).toContain(nextP2Card);
+    expect(game.cardPot.suit).toBe(p1HighestValueCard.suit);
+    for (const player of game.players) {
+        expect(player.cards).not.toContain(nextP2Card);
+    }
+    expect(p1.cards).toHaveLength(7);
+    expect(p2.cards).toHaveLength(7);
+    expect(p3.cards).toHaveLength(8);
+    expect(game.currentPlayer).toBe(p3);
+
+    const nextP3Card = findValidCard(p3.cards, nextP2Card.suit);
+
+    game.sendMove(nextP3Card);
+    for (const player of game.players) {
+        expect(player.cards).not.toContain(nextP3Card);
+    }
+
+    const playedCards = [p1HighestValueCard, nextP2Card, nextP3Card] as const;
+
+    const bestCard = playedCards.reduce((prevCard, card) =>
+        cardCompare(prevCard, card, p1HighestValueCard.suit) > 0
+            ? card
+            : prevCard
+    );
+
+    const bestCardIndex = playedCards.indexOf(bestCard);
+    expect(bestCardIndex).not.toBe(-1);
+    expect(originalPlayers[bestCardIndex]).toBe(game.players[0]);
 });
